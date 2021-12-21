ea026973e2bf6d0407d016c941e12543c0d6ae81
Raymond Wentowski <raymond.wentowski@duke-energy.com>
2021-12-20 23:21:06 -0500

    Modifications to Program.cs.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace ReleaseWebServer
@@ -8,15 +10,33 @@ namespace ReleaseWebServer
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            var config = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+            .Build();
+
+            var host = new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        //    public static IHostBuilder CreateHostBuilder(string[] args) =>
 
+        //        new WebHostBuilder().UseConfiguration()
+        //        //Host.CreateDefaultBuilder(args).
+        //        //    .ConfigureWebHostDefaults(webBuilder =>
+        //        //    {
+        //        //        webBuilder.UseStartup<Startup>();
+        //        //    });
+
+
+        //}
     }
 }
